208f139fcd91bc3cf03be7da7d05808519a93f0f
PengboWang <wpb504@126.com>
2020-06-08 14:23:24 +0800

    ui

--- app/src/main/java/com/example/clfilter/FirstFragment.kt
+++ app/src/main/java/com/example/clfilter/FirstFragment.kt
@@ -1,21 +1,29 @@
 package com.example.clfilter
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
+    private val baseUrl = "https://t66y.com/thread0806.php?fid=22"
+    private lateinit var getDataJob: Job
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
@@ -26,6 +34,33 @@ class FirstFragment : Fragment() {
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        btn_refresh.setOnClickListener { refreshData() }
+        getData()
+    }
+
+    private fun refreshData() {
+        release()
+        getData()
+    }
+
+    private fun getData() {
+        getDataJob = GlobalScope.launch(Dispatchers.IO) {
+            val base = Jsoup.connect(baseUrl).get().body()
+            val main = base.getElementById("main")
+            Log.d("apollo", "main: $main")
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        release()
+    }
+
+    private fun release() {
+        if (getDataJob.isActive && !getDataJob.isCancelled) {
+            getDataJob.cancel()
         }
     }
 }
