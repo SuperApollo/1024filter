1aff3eab64599f7c8364e1270386e946fb83e481
PengboWang <wpb504@126.com>
2020-06-08 16:06:18 +0800

    开始解析列表

--- app/src/main/java/com/example/clfilter/FirstFragment.kt
+++ app/src/main/java/com/example/clfilter/FirstFragment.kt
@@ -14,13 +14,18 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
+import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-    private val baseUrl = "https://t66y.com/thread0806.php?fid=22"
+    private val baseUrl = "https://t66y.com/index.php"
     private lateinit var getDataJob: Job
+    private val handling = AtomicBoolean(false)
+    private var currentPage = 0
+    private val onlineBeans: MutableList<OnlineBean> = mutableListOf()
+    private var maxItems = 10
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -46,15 +51,57 @@ class FirstFragment : Fragment() {
     }
 
     private fun getData() {
+        if (handling.get()) {
+            return
+        }
+        progress.visibility = View.VISIBLE
         getDataJob = GlobalScope.launch(Dispatchers.IO) {
-            val base = Jsoup.connect(baseUrl).get().body()
-            val main = base.getElementById("main")
-            Log.d("apollo", "main: $main")
+            try {
+                val index = Jsoup.connect(baseUrl).get().body()
+                val indexMain = index.getElementById("main")
+                val child = indexMain.child(1)
+                val cate1 = child.getElementById("cate_1")//主页
+                val onlineVideo = cate1.child(8)//在线yp板块
+                val target = onlineVideo.child(1).allElements
+                val topics = onlineVideo.child(2).getElementsByClass("f12")
+                val articles = onlineVideo.child(3).getElementsByClass("f12")
+                launch(Dispatchers.Main) {
+                    tv_info.text = "主题: ${topics.text()}, 文章: ${articles.text()}"
+                }
+                val link = target.select("a").first()
+                val absHref = link.attr("abs:href")
+                parseOnlineVideoPage(absHref)
+                launch(Dispatchers.Main) { progress.visibility = View.GONE }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                launch(Dispatchers.Main) { progress.visibility = View.GONE }
+                handling.compareAndSet(true, false)
+            }
         }
     }
 
-    override fun onPause() {
-        super.onPause()
+    private fun parseOnlineVideoPage(absHref: String) {
+//        while (true) {
+//            if (onlineBeans.size > maxItems) {
+//                break
+//            }
+//            if (getDataJob.isCancelled) {
+//                break
+//            }
+            currentPage++
+            Log.d("apollo", "currentPage: $currentPage")
+            val pageUrl = "$absHref&search=&page=$currentPage"
+            val page = Jsoup.connect(pageUrl).get().body()
+            val main = page.getElementById("main")
+            val listElement = main.child(4)
+
+
+            Log.d("apollo", listElement.text())
+//        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
         release()
     }
 
---
+++ app/src/main/java/com/example/clfilter/OnlineBean.kt
@@ -0,0 +1,3 @@
+package com.example.clfilter
+
+data class OnlineBean(val name: String, val url: String, val comments: String)
