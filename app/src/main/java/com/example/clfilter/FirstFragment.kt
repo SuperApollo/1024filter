468d440397559fd72dee1253dac8148a7eb6cef3
PengboWang <wpb504@126.com>
2020-06-12 16:04:15 +0800

    震动，ua，延时反爬

--- app/src/main/java/com/example/clfilter/FirstFragment.kt
+++ app/src/main/java/com/example/clfilter/FirstFragment.kt
@@ -1,9 +1,12 @@
 package com.example.clfilter
 
+import android.app.Service
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -16,10 +19,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_first.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import java.util.concurrent.atomic.AtomicBoolean
@@ -38,6 +38,22 @@ class FirstFragment : BaseFragment(), OnItemLongClickListener {
     private var commentsLimit = 3
     lateinit var myAdapter: MyAdapter
     private var showErrorTime = 0
+    val my_headers = arrayOf(
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14",
+        "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11",
+        "Opera/9.25 (Windows NT 5.1; U; en)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)",
+        "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12",
+        "Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9",
+        "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7",
+        "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0 "
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -89,7 +105,10 @@ class FirstFragment : BaseFragment(), OnItemLongClickListener {
         progress.visibility = View.VISIBLE
         getDataJob = GlobalScope.launch(Dispatchers.IO) {
             try {
-                val index = Jsoup.connect(baseUrl).get().body()
+                val index = Jsoup.connect(baseUrl)
+                    .headers(getHeaders())
+                    .get()
+                    .body()
                 val indexMain = index.getElementById("main")
                 val child = indexMain.child(1)
                 val cate1 = child.getElementById("cate_1")//主页
@@ -121,7 +140,7 @@ class FirstFragment : BaseFragment(), OnItemLongClickListener {
         }
     }
 
-    private fun parseOnlineVideoPage(absHref: String) {
+    private suspend fun parseOnlineVideoPage(absHref: String) {
         while (true) {
             if (getDataJob.isCancelled) {
                 break
@@ -132,12 +151,27 @@ class FirstFragment : BaseFragment(), OnItemLongClickListener {
             currentPage++
             Log.d("apollo", "currentPage: $currentPage")
             val pageUrl = "$absHref&search=&page=$currentPage"
-            val page = Jsoup.connect(pageUrl).get().body()
+            val page = Jsoup.connect(pageUrl)
+                .headers(getHeaders())
+                .get()
+                .body()
             val main = page.getElementById("main")
             val listElement = main.child(4).child(0)
             val realList = listElement.child(1)
             parseLine(realList)
+            delay(100)
         }
+    }
+
+    private fun getHeaders(): MutableMap<String, String>? {
+        val header = mutableMapOf<String, String>()
+//        header["Host"] = "https://t66y.com"
+        header["User-Agent"] = my_headers.random()
+        header["Accept"] = "  text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+        header["Accept-Language"] = "zh-cn,zh;q=0.5"
+        header["Accept-Charset"] = "  GB2312,utf-8;q=0.7,*;q=0.7"
+        header["Connection"] = "keep-alive"
+        return header
     }
 
     private fun parseLine(realList: Element) {
@@ -196,8 +230,14 @@ class FirstFragment : BaseFragment(), OnItemLongClickListener {
     }
 
     override fun onItemLongClick(position: Int, item: OnlineBean) {
-        ToastUtil.s(context, "复制成功!")
         copy(item.url)
+        ToastUtil.s(context, "复制成功!")
+        vibrate()
+    }
+
+    private fun vibrate() {
+        val vib = requireContext().getSystemService(Service.VIBRATOR_SERVICE) as Vibrator
+        vib.vibrate(50)
     }
 
     //复制
